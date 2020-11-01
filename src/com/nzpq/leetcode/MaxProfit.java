4f2d0660b2ecc8a79c49b99d80ccfff4ace6c804
nzpq <nzpq@foxmail.com>
2020-11-01 10:29:52 +0800

    买卖股票的最佳时机 II

---
+++ src/com/nzpq/leetcode/MaxProfit.java
@@ -0,0 +1,40 @@
+package com.nzpq.leetcode;
+
+/**
+ * @author 鹏
+ * @date 2020-11-01 10:20
+ *
+ * 买卖股票的最佳时机 II
+ * 给定一个数组，它的第 i 个元素是一支给定股票第 i 天的价格。
+ * 设计一个算法来计算你所能获取的最大利润。你可以尽可能地完成更多的交易（多次买卖一支股票）。
+ * 注意：你不能同时参与多笔交易（你必须在再次购买前出售掉之前的股票）。
+ * 示例 1:
+ * 输入: [7,1,5,3,6,4]
+ * 输出: 7
+ * 解释: 在第 2 天（股票价格 = 1）的时候买入，在第 3 天（股票价格 = 5）的时候卖出, 这笔交易所能获得利润 = 5-1 = 4 。
+ *      随后，在第 4 天（股票价格 = 3）的时候买入，在第 5 天（股票价格 = 6）的时候卖出, 这笔交易所能获得利润 = 6-3 = 3 。
+ *
+ * 作者：力扣 (LeetCode)
+ * 链接：https://leetcode-cn.com/leetbook/read/top-interview-questions-easy/x2zsx1/
+ * 来源：力扣（LeetCode）
+ * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+ */
+public class MaxProfit {
+
+    private static int maxProfit(int[] prices){
+
+        int result = 0;
+        for (int i = 0; i < prices.length - 1; i++) {
+            int temp = prices[i+1] - prices[i];
+            if(temp > 0){
+                result += temp;
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int[] prices = {7,1,5,3,6,4};
+        System.out.println(maxProfit(prices));
+    }
+}
